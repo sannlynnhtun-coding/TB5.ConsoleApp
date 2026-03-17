92a96667015478c4381b09a0741da8d43be4b0b7
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2026-03-17 23:27:13 +0630

    feat: Add initial EF Core sample application including database context and TblSale entity.

---
+++ TB5.ConsoleApp.EFCoreSample/EFCoreSample/EFCoreService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TB5.ConsoleApp.Database.AppDbContextModels;
+
+namespace TB5.ConsoleApp.EFCoreSample.EFCoreSample
+{
+    public class EFCoreService
+    {
+        private readonly AppDbContext db = new AppDbContext();
+
+        public void Read()
+        {
+            List<TblProduct> lst = db.TblProducts.ToList();
+
+            foreach (TblProduct item in lst)
+            {
+                Console.WriteLine(item.Id);
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Price);
+            }
+        }
+
+        public void Edit()
+        {
+            TblProduct? itemProduct = db.TblProducts.Where(x => x.Id == 1).FirstOrDefault();
+            if (itemProduct is null)
+            {
+                Console.WriteLine("Product not found.");
+                return;
+            }
+
+            Console.WriteLine(itemProduct.Id);
+            Console.WriteLine(itemProduct.Name);
+            Console.WriteLine(itemProduct.Price);
+        }
+
+        public void Create()
+        {
+            TblProduct product = new TblProduct()
+            {
+                Name = "test",
+                Price = 10000
+            };
+            db.TblProducts.Add(product);
+            int result = db.SaveChanges();
+            string message = result > 0 ? "Product created successfully." : "Failed to create product.";
+            Console.WriteLine(message);
+        }
+
+        public void Update()
+        {
+            TblProduct? itemProduct = db.TblProducts.Where(x => x.Id == 1).FirstOrDefault();
+            if (itemProduct is null)
+            {
+                Console.WriteLine("Product not found.");
+                return;
+            }
+
+            itemProduct.Price = 2000;
+            int result = db.SaveChanges(); // update
+            string message = result > 0 ? "Product updated successfully." : "Failed to update product.";
+            Console.WriteLine(message);
+        }
+
+        public void Delete()
+        {
+            TblProduct? itemProduct = db.TblProducts.Where(x => x.Id == 1).FirstOrDefault();
+            if (itemProduct is null)
+            {
+                Console.WriteLine("Product not found.");
+                return;
+            }
+
+            db.TblProducts.Remove(itemProduct);
+            int result = db.SaveChanges(); // update
+            string message = result > 0 ? "Product deleted successfully." : "Failed to delete product.";
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ TB5.ConsoleApp.EFCoreSample/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+using TB5.ConsoleApp.EFCoreSample.EFCoreSample;
+
+Console.WriteLine("Hello, World!");
+
+EFCoreService service = new EFCoreService();
+service.Create();
+service.Read();
+service.Update();
+service.Edit();
+service.Delete();
+
+
+Console.ReadLine();
+
+//AppDbContext db = new AppDbContext();
+//List<TblProduct> lst = db.TblProducts.ToList();
+
+//foreach (TblProduct item in lst)
+//{
+//    Console.WriteLine(item.Id);
+//    Console.WriteLine(item.Name);
+//    Console.WriteLine(item.Price);
+//}
+
+//TblProduct product = new TblProduct()
+//{
+//    Name = "test",
+//    Price = 10000
+//};
+//db.TblProducts.Add(product);
+//int result = db.SaveChanges();
+//string message = result > 0 ? "Product created successfully." : "Failed to create product.";
+//Console.WriteLine(message);
+
+//TblProduct? itemProduct = db.TblProducts.Where(x => x.Id == 1).FirstOrDefault();
+//if (itemProduct is null)
+//{
+//    Console.WriteLine("Product not found.");
+//    return;
+//}
+
+//itemProduct.Price = 2000;
+//db.SaveChanges(); // update
+
+
+//db.TblProducts.Remove(itemProduct);
+//db.SaveChanges();
